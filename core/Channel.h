f3c0f1e0df031db618f3933770eb290f40651163
yiwen <e0968880@u.nus.edu>
2024-12-18 01:33:02 +0000

    add header file for channel

---
+++ core/Channel.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "../common/TimeStamp.h"
+#include <sys/epoll.h>
+#include <functional>
+#include <memory>
+
+class EventLoop;
+class Channel {
+	public:
+		using CallBack = std::function<void()>;
+		using ReadCallBack = std::function<void(TimeStamp)>;
+		
+		Channel(EventLoop *loop, int fd);
+		~Channel();
+		
+		void onEvent(TimeStamp t);
+		
+		void onRead(ReadCallBack f) {_onRead = f;}
+		void onWrite(CallBack f) {_onWrite = f;}
+		void onClose(CallBack f) {_onClose = f;}
+		void onError(CallBack f) {_onError = f;}
+		
+		void enableRead() {_enable(_readEvent);}
+		void enableWrite() {_enable(_writeEvent);}
+		void disableRead() {_disable(_readEvent);}
+		void disableWrite() {_disable(_writeEvent);}
+
+
+	private:
+		int _events;
+		int _revents;
+		ReadCallBack _onRead;
+		CallBack _onWrite;
+		CallBack _onClose;	
+		CallBack _onError;
+		const int _noneEvent = 0;
+		const int _readEvent = EpollIn | EPOLLPRI;
+		const int _writeEvent = EPOLLOUT;
+		void _enable(int flag) {
+			_events |= flag; 
+			update();
+		}
+		void _disable(int flag) {
+			_events &= ~flag;
+			update();
+		}
+};
+		
